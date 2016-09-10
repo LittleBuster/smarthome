c18c66b2a42ab625704e5dcfc340e28062741722
LittleBuster <DenisovS21@gmail.com>
2016-09-10 12:34:38 +0700

    add license

--- house/cam.h
+++ house/cam.h
@@ -1,3 +1,14 @@
+/* Smart Home: House server
+ *
+ * Copyright (C) 2016 Sergey Denisov.
+ * Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence 3
+ * as published by the Free Software Foundation; either version 3
+ * of the Licence, or (at your option) any later version.
+ */
+
 #ifndef __CAM_H__
 #define __CAM_H__
 
